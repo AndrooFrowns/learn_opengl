3e36e4673cf685af9b6961445c01a537a93316d5
Andrew <AndrewFrauens@users.noreply.github.com>
2023-03-19 02:29:38 -0400

    revise the message that comes up with no input to actually show all the available exercises.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod lister;
 struct Args {
     /// Either CHAPTER.SECTION or NAME of exercise to run
     #[arg()]
-    arg: String,
+    arg: Option::<String>,
 }
 
 
@@ -17,27 +17,35 @@ fn main() {
     let arg = Args::parse().arg;
     let runner_list = lister::Lister::new();
 
-    let id = if arg.contains('.') {
-        let args: Vec<&str> = arg.split('.').collect();
-        let chapter = args[0].parse::<i32>().expect("Invalid argument including string but not convertable to ints");
-        let section = args[1].parse::<i32>().expect("Invalid argument including string but not convertable to ints");
+    if let Some(arg) = arg {
+        let id = if arg.contains('.') {
+            let args: Vec<&str> = arg.split('.').collect();
+            let chapter = args[0].parse::<i32>().expect("Invalid argument including string but not convertable to ints");
+            let section = args[1].parse::<i32>().expect("Invalid argument including string but not convertable to ints");
 
-        RunID::Numeric { chapter, section }
-    } else {
-        RunID::Named(arg)
-    };
+            RunID::Numeric { chapter, section }
+        } else {
+            RunID::Named(arg)
+        };
 
-    let run_result = runner_list.launch(id);
+        let run_result = runner_list.launch(id);
 
-    match run_result {
-        Ok(_) => { println!("========= DONE ==========") }
-        Err(_) => {
-            println!("ID not found");
-            println!("usage: <arg>");
-            println!("arg: CHAPTER.SECTION or NAME");
-            println!();
-            println!("Available Options:");
-            println!("{}", runner_list);
+        match run_result {
+            Ok(_) => { println!("========= DONE ==========") }
+            Err(_) => {
+                printHelpOptions(&runner_list);
+            }
         }
+    } else {
+        printHelpOptions(&runner_list);
     }
+}
+
+fn printHelpOptions(list: &lister::Lister) {
+    println!("ID not found");
+    println!("usage: <arg>");
+    println!("arg: CHAPTER.SECTION or NAME");
+    println!();
+    println!("Available Options:");
+    println!("{}", list);
 }
